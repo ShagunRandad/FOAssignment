c1bcae0c3adee1ea90cab5e8706992e6fb6fdc4d
Shagun Randad (EXT-Nokia) <shagun.randad.ext@nokia.com>
2024-09-09 10:40:26 +0530

    Initial commit

---
+++ src/fodockerassignment/DemoClass.java
@@ -0,0 +1,12 @@
+package fodockerassignment;
+
+public class DemoClass {
+	public static void printSystemProperties() {
+		System.out.println("System Properties are :"+System.getProperties());
+	}
+public static void main(String[] args) {
+	System.out.println("Lets Print System Properties: ");
+	printSystemProperties();
+	
+}
+}
